939b960b161994148cd61bd24a4f698d22c9aa65
abstractsig <>
2020-02-11 16:39:47 +1300

    implement nrf52840

--- io_board.h
+++ io_board.h
@@ -7,7 +7,12 @@
 #define io_board_H_
 #include <io_cpu.h>
 
-
+#ifdef IMPLEMENT_IO_BOARD
+void
+add_io_implementation_board_methods (io_implementation_t *io_i) {
+	add_io_implementation_cpu_methods (io_i);
+}
+#endif /* IMPLEMENT_IO_BOARD */
 #endif
 /*
 ------------------------------------------------------------------------------
